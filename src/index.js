bdd8c8bd53732ba67a6992ab45a8148c5e2a2261
Gleason <bianliuzhu@gmail.com>
2019-04-24 17:44:09 +0800

    通过 import 方式引入 lodash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+
+// import lodash
+import _ from 'lodash'
+
+
 /**
  * 创建 函数 component
  */
 function component() {
   let element = document.createElement('div')
 
-  // lodash (目前通过一个 script 脚本引入) 对于执行这一项必须的
+  // - lodash (目前通过一个 script 脚本引入) 对于执行这一项必须的
+  // lodash, now imported by this script(lodash 现在由脚本引入)
   element.innerHTML = _.join(['hello', 'webpack'], '')
 
   return element
