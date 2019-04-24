315345a0778562aec4c02fbc1790ef2e3776ff6d
Gleason <bianliuzhu@gmail.com>
2019-04-24 18:17:16 +0800

    更新 index.html 执行 npx webpack 指令 生成 /dist/main.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ *  显式要求引入的 lodash 必须存在，然后将它绑定为 _（没有全局作用域污染）。
+ *  通过声明模块所需的依赖，webpack 能够利用这些信息去构建依赖图，
+ *  然后使用图生成一个优化过的，会以正确顺序执行的 bundle。
+ */
 
 // import lodash
 import _ from 'lodash'
@@ -11,7 +16,7 @@ function component() {
 
   // - lodash (目前通过一个 script 脚本引入) 对于执行这一项必须的
   // lodash, now imported by this script(lodash 现在由脚本引入)
-  element.innerHTML = _.join(['hello', 'webpack'], '')
+  element.innerHTML = _.join(['hello ', ' webpack'], ' ')
 
   return element
 }
