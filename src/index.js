c3be8047d283272b136d94cb93c199fee30d4519
Gleason <bianliuzhu@gmail.com>
2019-04-25 17:11:36 +0800

    1. 删除多余 index.js 文件 2. 问题: build 后 页面没有显示 3. 原因: dist/index.html 脚本引入错误改为 bundle.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function component() {
 
   // - lodash (目前通过一个 script 脚本引入) 对于执行这一项必须的
   // lodash, now imported by this script(lodash 现在由脚本引入)
-  element.innerHTML = _.join(['hello ', ' webpack'], ' ')
+  element.innerHTML = _.join(['hello', 'webpack'], ' ')
   element.classList.add('hello');
 
   return element
