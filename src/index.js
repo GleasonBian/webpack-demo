0551246ea93fcec14c4a51119d320099a327274a
Gleason <bianliuzhu@gmail.com>
2019-04-26 10:39:18 +0800

    删除 引用无效代码

--- src/index.js
+++ src/index.js
@@ -5,27 +5,15 @@
  */
 
 import _ from 'lodash'
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml'
+
 /**
  * 创建 函数 component
  */
 function component() {
   let element = document.createElement('div')
 
-  // - lodash (目前通过一个 script 脚本引入) 对于执行这一项必须的
   // lodash, now imported by this script(lodash 现在由脚本引入)
   element.innerHTML = _.join(['hello', 'webpack'], ' ')
-  element.classList.add('hello');
-
-  // 将图像添加到现有div
-  let myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon)
-  
-  console.log(Data);
 
   return element
 }
