efb80757cda67382f956a4bc199dac5d3bf4cb20
Gleason <bianliuzhu@gmail.com>
2019-04-26 14:05:31 +0800

    1. 安装 clean-webpack-plugin 2. 更新 webpack.config.js 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path')
 // HtmlWebPackPlugin 插件解决 index.html 文件仍然会引用旧的名字问题
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 每次构建前清理dist文件夹
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output management'
     })
