f68fda01aa52d6eb67befa9f585b725537621c53
Gleason <bianliuzhu@gmail.com>
2019-04-26 14:56:38 +0800

    1.安装  clean-webpack-plugin 2.纠正 clean-webpack-plugin@2.0.1 插件写法 3.安装 papaparse

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,18 @@ const path = require('path')
 // HtmlWebPackPlugin 插件解决 index.html 文件仍然会引用旧的名字问题
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // 每次构建前清理dist文件夹
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output management'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
