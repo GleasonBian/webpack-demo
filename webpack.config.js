f6559af03fcf6face00225936d6b6645b7be0149
Gleason <bianliuzhu@gmail.com>
2019-04-26 13:41:22 +0800

    更新webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
-
+// HtmlWebPackPlugin 插件解决 index.html 文件仍然会引用旧的名字问题
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output management'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
