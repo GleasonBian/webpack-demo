99aaa6db4c1520d037048f3db0eaab4aa04f4ea1
Gleason <bianliuzhu@gmail.com>
2019-04-29 11:01:06 +0800

    增加 math.js 定义 乘方 和 立方 两个函数

---
+++ src/math.js
@@ -0,0 +1,21 @@
+
+
+
+
+/**
+ * @export square
+ * @param {*} x
+ * @returns x的乘方
+ */
+export function square(x) { 
+  return x * x;
+}
+
+/**
+ * @export cube
+ * @param {*} x
+ * @returns x 的立方
+ */
+export function cube(x) {
+  return x * x * x;
+}
